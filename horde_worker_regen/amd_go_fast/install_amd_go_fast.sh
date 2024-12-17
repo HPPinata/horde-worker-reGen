8714d868b68f5d96244f28b4df92ce43a5e3a3ef
HPPinata <83947761+HPPinata@users.noreply.github.com>
2024-12-17 16:33:59 +0100

    delete node
    only install flash_attn

--- horde_worker_regen/amd_go_fast/install_amd_go_fast.sh
+++ horde_worker_regen/amd_go_fast/install_amd_go_fast.sh
@@ -6,11 +6,7 @@ if [ "${FLASH_ATTENTION_TRITON_AMD_ENABLE^^}" == "TRUE" ]; then
 		echo "Tried to install flash attention and failed!"
 	else
 		echo "Installed flash attn."
-		if ! cp horde_worker_regen/amd_go_fast/amd_go_fast.py "${PY_SITE_DIR}"/hordelib/nodes/; then
-			echo "Failed to install AMD GO FAST."
-		else
-			echo "Installed AMD GO FAST."
-		fi
+		rm -f "${PY_SITE_DIR}"/hordelib/nodes/amd_go_fast.py
 	fi
 else
 	echo "Did not detect support for AMD GO FAST. Cleaning up."
